5d95d8ff39e1977debd08b42f228d48728a39872
Busra <busramacak@outlook.com>
2023-06-21 16:52:55 +0300

    splash screen active

--- app/src/main/java/com/bmprj/weatherforecast/SplashActivity.kt
+++ app/src/main/java/com/bmprj/weatherforecast/SplashActivity.kt
@@ -1,11 +1,37 @@
 package com.bmprj.weatherforecast
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    val cs = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        cs.launch {
+            delay(2000)
+            startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+            finish()
+        }
+    }
+
+
+    override fun onPause() {
+        cs.cancel()
+        super.onPause()
     }
 }
